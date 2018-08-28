834c4cb552b681f4ef4987e8eef4324b531f3ee4
Taylor Williams <taylor@thewilliams.net>
2018-08-28 00:37:24 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require_relative './slugify.rb'
 class Artist < ActiveRecord::Base
   extend Slugify
@@ -7,9 +6,6 @@ class Artist < ActiveRecord::Base
     self.slug = self.class.make_slug(name)
   end
 
-=======
-class Artist < ActiveRecord::Base
->>>>>>> 65ead0c298f53a8507a764ced7af5e09964f0f20
   has_many :songs
   has_many :genres, through: :songs
 end
